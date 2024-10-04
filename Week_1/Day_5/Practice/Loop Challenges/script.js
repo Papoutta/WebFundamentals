623e018a1633af84297c12699ab54cfaf3f9d439
farouk ghezal <faroukghezal3@gmail.com>
2024-10-04 13:58:55 +0100

    Loop Challenges

---
+++ Week_1/Day_5/Practice/Loop Challenges/script.js
@@ -0,0 +1,38 @@
+// Task 1 
+
+for (var i = 1 ; i <= 20 ; i++) {
+    if ( i%2 == 0 ) {
+        console.log(i)
+    }
+}
+
+// Task 2
+
+for (var i = 100 ; i>=0 ; i--) {
+    if ( i%3 == 0 ) {
+        console.log(i)
+    }
+}
+
+// Task 3 
+
+var x = 4;
+for (var i = 0; i < 6; i++) {
+    console.log(x)
+    x = x -1.5
+}
+
+//Task 4 
+sum = 0
+for (var i =0 ; i <= 100 ; i++) {
+    sum = sum + i
+}
+console.log(sum)
+
+//task 5 
+
+fac = 1
+for (var i =1 ; i <= 12 ; i++) {
+    fac = fac * i
+}
+console.log(fac)
