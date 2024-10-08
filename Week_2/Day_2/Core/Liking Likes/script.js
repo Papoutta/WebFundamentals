382ae68d3438e5907c56715a817f4d8c660a2d59
farouk ghezal <faroukghezal3@gmail.com>
2024-10-08 11:23:38 +0100

    liking likes

---
+++ Week_2/Day_2/Core/Liking Likes/script.js
@@ -0,0 +1,27 @@
+var count = 0;
+function likes(){
+    var number = document.getElementById("likes-nbr")
+    count += 1
+    number.innerHTML = count
+}
+
+var count1 = 0
+function likes1(){
+    var number = document.getElementById("likes-nbr1")
+    count1 += 1
+    number.innerHTML = count1
+}
+
+var count2 = 0
+function likes2(){
+    var number = document.getElementById("likes-nbr2")
+    count2 += 1
+    number.innerHTML = count2
+}
+
+var count3 = 0
+function likes3(){
+    var number = document.getElementById("likes-nbr3")
+    count3 += 1
+    number.innerHTML = count3
+}
